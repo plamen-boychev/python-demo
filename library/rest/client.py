c105e76abc015361df907fbd0791bfdb1238c074
Plamen Boychev <pboychev@vmware.com>
2022-06-26 20:42:21 +0300

    REST library - introduced authorizatiom mechanism.
    Enables different authorization mechanisms and schemes to be used for REST API consumption.
    
    References: #1

--- library/rest/client.py
+++ library/rest/client.py
@@ -1,4 +1,5 @@
 import request
+import auth
 
 class Client:
     """A base service client class definition.
@@ -6,37 +7,45 @@ class Client:
 
     base_path = None
     headers = None
+    auth = None
 
-    def __init__(self, base_path, headers=None):
+    def __init__(self, base_path, headers=None, auth=None):
         """Initializing a client."""
+        if auth and False == isinstance(auth, auth.Auth):
+            raise Exception("Authorization handler needs to be an instance of auth.Auth class, {} provided instead".format(type(auth)))
+
         self.base_path = base_path
         self.headers = headers if headers else None
+        self.auth = auth if auth else None
 
     def get(self, path, headers=None):
         """Execute GET requests."""
-        return self.execute_request(self.build_request(path, request.HTTPVerb.get, headers=headers));
+        return self.handle_request(path, request.HTTPVerb.get, headers=headers);
 
     def post(self, path, data, headers=None):
         """Execute POST requests."""
-        return self.execute_request(self.build_request(path, request.HTTPVerb.post, data=data, headers=headers));
+        return self.handle_request(path, request.HTTPVerb.post, data=data, headers=headers);
 
     def put(self, path, data, headers=None):
         """Execute PUT requests."""
-        return self.execute_request(self.build_request(path, request.HTTPVerb.put, data=data, headers=headers));
+        return self.handle_request(path, request.HTTPVerb.put, data=data, headers=headers);
 
     def patch(self, path, data, headers=None):
         """Execute PATCH requests."""
-        return self.execute_request(self.build_request(path, request.HTTPVerb.patch, data=data, headers=headers));
+        return self.handle_request(path, request.HTTPVerb.patch, data=data, headers=headers);
 
     def delete(self, path, headers=None):
         """Execute DELETE requests."""
-        return self.execute_request(self.build_request(path, request.HTTPVerb.delete, headers=headers));
+        return self.handle_request(path, request.HTTPVerb.delete, headers=headers);
 
     def build_request(self, path, method, data=None, headers=None):
         """Creating request objects."""
         full_path = self.base_path + path
         return request.Request(method, path, data, headers)
 
-    def execute_request(self, request):
+    def handle_request(self, path, method, data=None, headers=None):
         """Executing a previously built request object."""
-        return request.invoke_request()
+        req = self.build_request(path, request.HTTPVerb.delete, headers=headers);
+        if self.auth:
+            self.auth.decorate_request(req)
+        return req.invoke_request()
