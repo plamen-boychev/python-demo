e00ce4520715151bffa84e9d36475e7bd615b01f
Plamen Boychev <plamen.boychev@gmail.com>
2022-07-01 10:19:41 +0300

    Retry mechnism polishing.
    The old implementation was prompt to stack overflow errors.
    There should be no such case with the new architecture, but it still depends on the retry condition classes.

--- library/github/githubclient.py
+++ library/github/githubclient.py
@@ -1,7 +1,7 @@
 import time
 from library.rest.auth import Auth, TokenAuth
 from library.rest.error_handling import ErrorHandler
-from library.rest.client import Client as BaseClient
+from library.rest.client import Client as BaseClient, RetryCondition
 from library.rest.request import Request
 from library.rest.response import Response
 
@@ -16,39 +16,30 @@ class GitHubClient(BaseClient):
         headers["Accept"] = "application/vnd.github.v3+json"
         # TODO: Verify credentials are valid
         auth = auth if auth else TokenAuth(token)
-        error_handler = error_handler if error_handler else GitHubErrorHandler()
-        super().__init__(base_path, headers, auth, error_handler)
+        super().__init__(base_path, headers, auth, error_handler, GitHubRetryCondition())
 
     def fetch_user_details(self, user_login:str):
         """Enables fetching a user details by its username / login."""
         # TODO: Validate input
         return self.get("/users/{}".format(user_login));
 
-class GitHubErrorHandler(ErrorHandler):
-    """Handler for API errors for the GitHub REST API service."""
+class GitHubRetryCondition(RetryCondition):
+    """Detects retry errors for the GitHub REST API service, indicates retry is required."""
 
     # TODO: Make retries configurable
     max_retries = 5
     retries = 0
 
-    # TODO: Avoid:
-    #       - Passing the client as a parameter
-    #       - Recusrion - possible stack overflow
-    #       - Create a reusable rate limit mechanism for both GitHub and Freshdesk clients -
-    #         the only difference is in the calculation of delay before next attempt
-    def handle_error(self, response: Response, request:Request, client:BaseClient):
+    def is_retry_required(self, response: Response):
         """Detects an error in the response. If would not raise an exception should return a response object"""
-        if response.status_code >= 400:
-            if "X-RateLimit-Remaining" in response.headers and "X-RateLimit-Reset" in response.headers and 0 == response.headers["X-RateLimit-Remaining"]:
-                if self.retries > self.max_retries:
-                    raise Exception("Too many unsuccessful attempts to execute the request!")
-                now = int(round(time.time()))
-                retry_after = response.headers["X-RateLimit-Reset"] - now
-                # TODO: Log details in a proper way - should not be visible when running tests
-                # print("Rate limit exceeded. Will retry in {} seconds".format(retry_after))
-                time.sleep(retry_after)
-                self.retries += 1
-                return client.execute_request(request)
-            else:
-                return ErrorHandler.handle_error(self, response, request, client)
-        return response
+        if response.status_code >= 400 and "X-RateLimit-Remaining" in response.headers and "X-RateLimit-Reset" in response.headers and 0 == response.headers["X-RateLimit-Remaining"]:
+            if self.retries > self.max_retries:
+                raise Exception("Too many unsuccessful attempts to execute the request!")
+            now = int(round(time.time()))
+            retry_after = response.headers["X-RateLimit-Reset"] - now
+            # TODO: Log details in a proper way - should not be visible when running tests
+            # print("Rate limit exceeded. Will retry in {} seconds".format(retry_after))
+            time.sleep(retry_after)
+            self.retries += 1
+            return True
+        return False
